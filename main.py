9d23b1a5673801c4ffb732c09483d721ddbc66fe
Alí Sinisterra <haidarconh@gmail.com>
2025-08-30 17:14:07 -0500

    fix sleepy code

--- main.py
+++ main.py
@@ -2,7 +2,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/v1/puzzles/now/")
+@app.get("/")
+async def main():
+    return "Hello!"
+
+@app.get("/v1/puzzles/latest")
+async def fetchCurrentPuzzle():
+    return {
+        "snippets": [
+            {
+                "content": 'I bet on {losing} dogs',
+                "source": '"Puberty"',
+                "by": 'Mitski',
+                "date": '2016',
+            },
+            {
+                "content": "I'm {losing} my temper",
+                "source": 'Trust me bro',
+                "by": 'Me',
+                "date": '2025',
+            },
+        ],
+        "solution": 'losing',
+    }
+
+
+@app.get("/v1/puzzles/")
 async def fetchPuzzle(time: str|int):
     return {
         "snippets": [
