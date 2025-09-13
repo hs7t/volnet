98ed207809a1a69ed5b81b0582299112ac87b48e
Alí Sinisterra <haidarconh@gmail.com>
2025-09-13 09:35:40 -0500

    add similarity checker to API!!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from fastapi import FastAPI
+from wordalize import checkSimilarity
 
 app = FastAPI()
 
@@ -8,17 +9,26 @@ def readPuzzles():
         puzzles = json.load(puzzlesJSON)
     return puzzles
 
-@app.get("/")
-async def main():
-    return "Hello!"
-
-@app.get("/v1/puzzles/latest")
-async def fetchCurrentPuzzle():
+def readLatestPuzzle():
     puzzles = readPuzzles()
     latestPuzzle = next(reversed(puzzles.values()))
 
     return latestPuzzle
 
+@app.get("/")
+async def main():
+    return "Hello!"
+
+@app.get("/v1/puzzles/latest")
+async def returnLatestPuzzle():
+    return (await readLatestPuzzle())
+
+@app.get("/v1/similarity/")
+async def checkGuessCloseness(guess: str, solution: str|None = None): 
+    if solution == None:
+        solution = (await readLatestPuzzle())["word"]
+
+    return checkSimilarity(guess, solution)
 
 @app.get("/v1/puzzles/")
 async def fetchPuzzle(time: str|int):
